2175f283b9b52ab085a8c925e1a80a25ccf9ebce
Subhash-Yuvaraj <subhash28chml@gmail.com>
2024-06-17 20:47:28 +0530

    Infix and Operator overloading

--- src/Extension.kt
+++ src/Extension.kt
@@ -1,13 +1,20 @@
 fun main(){
-    var ani : Animal = Animal()
+    val ani : Animal = Animal()
     ani.diet="Herbivore"
-    var ani2:Animal = Animal()
+    val ani2:Animal = Animal()
     ani2.diet="Carnivore"
-    var newani=ani2.plus(ani)
+    val newani=ani2.plus(ani)
     println(newani.diet)
+    val ani3 : Animal = Animal()
+    ani3.diet="Herbivore"
+    val ani23:Animal = Animal()
+    ani23.diet="Carnivore"
+    val newani3=ani3 + ani23
+    println(newani3.diet)
 }
-fun Animal.plus(a:Animal) : Animal{
-    var animal:Animal = Animal()
+//infix takes only one param ->remember
+infix operator fun Animal.plus(a:Animal) : Animal{
+    val animal:Animal = Animal()
     animal.diet=this.diet+","+a.diet
     return animal
 }
